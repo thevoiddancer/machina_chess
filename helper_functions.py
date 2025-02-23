8fe98859ac8597de1095eeed4f45331b1d52b6e3
asimac <simac.andrea@gmail.com>
2025-02-23 22:30:23 +0100

    function project commit

--- helper_functions.py
+++ helper_functions.py
@@ -68,3 +68,13 @@ def tuple_to_koord(koord_tuple: tuple) -> str | None:
     koord_slova = slovo + broj
     return koord_slova
 
+board = [
+    ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
+    ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+    ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8']
+]
--- legal_functions.py
+++ legal_functions.py
@@ -1,21 +1,18 @@
-from helper_functions import koord_to_tuple, tuple_to_koord
+from helper_functions import koord_to_tuple, tuple_to_koord, board
 
 
-def legal_pijun(figura: dict[str, str], cilj: str) -> bool:
+def legal_kula (figura: dict[str, str], cilj: str) -> bool:
     """
-    Funkcija koja provjerava je li najavljeni pomak pijuna legalan. Pravila koja prati su:
-    - nije dopušten pomak u stranu
-    - nije dopušteno ostati na mjestu
-    - nije dopušteno pomak veći od 2 polja
-    - pomak od 2 polja je dopušten samo sa početne pozicije
-    - pomak od 1 polja je dopušten samo "naprijed", ovisno o boji
-    Funkcija ne testira mogućnost da se "pojede" protivnička figura. To je potrebno dodati.
-    Funkcija ne mijenja položaj figure, samo javlja je li novi položaj legalan.
+    Funkcija koja provjerava je li najavljeni pomak kule legalan. Pravila koja prati su:
+    + nije dopušteno ostati na mjestu 
+    + željeni potez je pravocrtan. (X ili Y koordinata je uvijek ista)
+    + potez je legalan ako između polazišta i odredišta nema niti jedne figure ili ako postoji suparnicka figura 
+
 
     Parameters
     ----------
     figura : dict[str, str]
-        Dictionary koji nosi podatke o figuri - boja, tip i mjesto.
+        Dictionary koji nosi podatke o figuri kula - boja, tip i mjesto.
     cilj : str
         Šahovski zapis položaja
     
@@ -31,44 +28,38 @@ def legal_pijun(figura: dict[str, str], cilj: str) -> bool:
     polazište = koord_to_tuple(figura['mjesto'])
     odredište = koord_to_tuple(cilj)
 
-    # Ako je kretanje u stranu automatski False
-    if polazište[0] != odredište[0]:
-        return False
+
     # Ako je polazište i odredište isto
-    # Gornja grana garantira da su 1. koordinate nadalje uvijek iste
-    elif polazište[1] == odredište[1]:
+    if polazište == odredište:
         return False
-    # Potez više od 2 je uvijek nedopušten
-    elif abs(odredište[1] - polazište[1]) > 2:
-        return False
-    # Potez od točno 2 je dopušten u jednom slučaju
-    elif abs(odredište[1] - polazište[1]) == 2:
-        # Startni red bijelih pijuna je 2
-        if figura['boja'] == 'bijelo' and polazište[1] == 2 and odredište[1] == 4:
-            return True
-        # Startni red crnih pijuna je 7
-        elif figura['boja'] == 'crno' and polazište[1] == 7 and odredište[1] == 5:
-            return True
-        # Inače je pomak od 2 koraka nedopušten
-        else:
-            return False
-    # Ostale su samo kombinacije jednog koraka
-    else:
-        # Bijele mogu ići samo naprijed
-        if figura['boja'] == 'bijelo' and odredište[1] == polazište[1] + 1:
-            return True
-        # Crne mogu ići samo nazad
-        elif figura['boja'] == 'crno' and odredište[1] == polazište[1] - 1:
-            return True
-        else:
-            return False
+
+    # Provjera pravocrtnog poteza (horizontalno ili vertikalno)
+    if polazište[0] == odredište[0]:  # Horizontalni potez
+        min_y, max_y = polazište[1], odredište[1]
+        if min_y > max_y:
+            min_y, max_y = max_y, min_y  # Zamjena ako je potrebno
+        for y in range(min_y + 1, max_y):
+            if board[polazište[0]][y] is not None:  
+                return False  # Figura blokira putanju
+
+    elif polazište[1] == odredište[1]:  # Vertikalni potez
+        min_x, max_x = polazište[0], odredište[0]
+        if min_x > max_x:
+            min_x, max_x = max_x, min_x  # Zamjena ako je potrebno
+        for x in range(min_x + 1, max_x):
+            if board[x][polazište[1]] is not None:  
+                return False  # Figura blokira putanju
+
+    return True  # Ako nema prepreka, potez je legalan
 
 # Ovdje ostale funkcije
+#Potrebne funkcije koja ce ako je potez legalan:
+#   -pojesti suparnicku figuru ako postoji na odredistu
+#   -maknuti pojedenu figuri s ploce
+#   -upisati novu vrijednost koordinate na listu i vratiti novu listu za board
 
 LEGAL_FUNCITONS = {
-    'pijun': legal_pijun,
-    # 'konj': legal_konj,
-    # ...
+    'kula': legal_kula
 }
 
 def is_move_legal(figura: dict[str, str], cilj) -> bool:
